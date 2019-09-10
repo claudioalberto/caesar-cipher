e8f98ac634aa7def89d85e20c86d8e6a4d1e236a
Claudio Alberto Faria Gonçalves <contato@claudioalberto.com.br>
2019-09-10 00:14:30 -0300

    Desenvolvimento da cifra de Cesar em python.

---
+++ caesar-cipher.py
@@ -0,0 +1,34 @@
+import sys
+
+ALPHABET = 'abcdefghijklmnopqrstuvxywz'
+KEY = 13
+
+def cypher(message, dir):
+  message_temp = ''
+  for m in message:
+    if m in ALPHABET:
+      c_index = ALPHABET.index(m)
+      message_temp += ALPHABET[(c_index + (dir * KEY)) % len(ALPHABET)]
+    else:
+      message_temp += m
+  return message_temp
+
+
+def encrypt(message):
+  return cypher(message, 1)
+
+def decrypt(message):
+  return cypher(message, -1)
+
+def main():
+  command = sys.argv[1].lower()
+  message = sys.argv[2].lower()
+  if command == 'encrypt':
+    print(encrypt(message))
+  elif command == 'decrypt':
+    print(decrypt(message))
+  else:
+    print(command + ' -> ERROR: Command not found')
+
+if __name__ == '__main__':
+  main()
